1a80ebd2b9e69c23f2ca99546da85807a9ba8a20
MarcFontaine <MarcFontaine@users.noreply.github.com>
2018-12-23 23:07:51 +0100

    add Text and JSON output

--- src/WsjtxToMqtt.hs
+++ src/WsjtxToMqtt.hs
@@ -4,18 +4,21 @@ module Main where
 
 import Control.Monad
 import System.Exit (exitFailure, exitSuccess)
-import qualified Data.ByteString.Lazy.Char8 as BSLC (pack)
+import qualified Data.ByteString.Lazy.Char8 as BSLC (pack,putStrLn)
 import qualified Data.ByteString.Char8 as BS (putStrLn)
+import qualified Data.Text.IO as Text (putStrLn)
 import qualified Options (runSubcommand, subcommand)
-import Data.Aeson as Aeson (eitherDecode)
-import Data.Yaml (encode)
+import Data.Aeson as Aeson (eitherDecode, encode)
+import Data.Yaml as Yaml (encode)
 import Paths_wsjtx_to_mqtt (version)
 import Data.Version (showVersion)
 import Network.Socket (withSocketsDo)
+
 import W2MTypes
 import Bridge (runBridge,testMqtt)
 import Config (getConfig, helpConfig)
-import WSJTX.UDP.Server (testDump, replyWithPackages)
+import WSJTX.UDP.Server (testDump, replyWithPackages, withWsjtxSocket, forkWsjtxServer)
+import WSJTX.UDP.NetworkMessage (Package(..), Decode(..))
 
 main :: IO ()
 main = withSocketsDo $ Options.runSubcommand [
@@ -29,8 +32,19 @@ main = withSocketsDo $ Options.runSubcommand [
 dumpWsjtx :: MainOptions -> DumpOptions -> [String] -> IO ()
 dumpWsjtx _ format _ = case format of
     DumpHaskell -> void $ testDump
-    DumpText    -> putStrLn "dumptxt"
-    DumpJSON    -> putStrLn "dumpJSON"
+    DumpText    -> server dumpText
+    DumpJSON    -> server $ BSLC.putStrLn . Aeson.encode
+    where
+        server :: (Package -> IO ()) -> IO ()
+        server dump = withWsjtxSocket wsjtxPort $ \sock -> do
+                        _threadId <- forkWsjtxServer sock dump
+                        void getLine
+          
+        wsjtxPort = 2237
+
+        dumpText :: Package -> IO ()
+        dumpText (PDecode (Decode {..})) = Text.putStrLn decode_message
+        dumpText _ = return ()
 
 showConfig :: MainOptions -> EmptyOptions -> [String] -> IO ()
 showConfig cmdOpts EmptyOptions _ = do
@@ -39,7 +53,7 @@ showConfig cmdOpts EmptyOptions _ = do
   putStrLn "---------------" 
   putStrLn "current config:"
   config <- getConfig cmdOpts
-  BS.putStrLn $ encode config
+  BS.putStrLn $ Yaml.encode config
   exitSuccess
 
 sendToUDP :: MainOptions -> EmptyOptions -> [String] -> IO ()
